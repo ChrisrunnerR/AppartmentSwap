e84f5ad24101cb1eb3a5ebe3af75b33dea583347
Christopher Stone <chrisrunner542@gmail.com>
2024-04-12 18:50:30 -0600

    moved files around

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:signin/pages/auth_page.dart';
+import 'package:signin/auth/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
